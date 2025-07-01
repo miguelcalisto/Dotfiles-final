0ea9e824275efa5d860f2ee4cbcef6878f0d34bb
miguelcalisto <miguelcalistors@gmail.com>
2025-07-01 11:51:00 -0300

    ajuste na estrutura dos scripts repetidas

--- scripts/scripts/battery.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-battery_status=$(acpi -b | grep -oP '\w+' | head -n1)
-battery_percentage=$(acpi -b | grep -oP '\d+%' | head -n1)
-echo " 🔋 <b>$battery_status $battery_percentage</b> "
-
